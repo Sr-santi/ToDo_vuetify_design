4689dd741d3acb54ed97d29c519ca3f848adbd07
Sr_santi <santiriosolaya@gmail.com>
2021-06-03 10:53:14 -0500

    Create the header and config initial files

---
+++ src/components/navBar.vue
@@ -0,0 +1,27 @@
+<template>
+    <nav>
+        <v-app-bar app flat color='#4CAF50'>
+            <v-toolbar-title class="text-uppercase">
+                <span class="font-weight-light">Todo</span>
+                <span>Sr-Santi</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn depressed color="#4CAF50">
+                <span>Sign out</span>
+                <v-icon right>mdi-location-enter</v-icon>
+            </v-btn>
+        </v-app-bar>
+    </nav>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.v-application {
+  background-color: #00a86b;
+}
+</style>
